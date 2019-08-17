6816b6ae6f0cc3f906a77147c3821d07c9d59c53
nbdoc <anhusky@163.com>
2019-08-17 16:26:20 +0800

    定稿demo

--- src/com/uu/a_proxy/jdk_proxy/UserServiceImp.java
+++ src/com/uu/a_proxy/jdk_proxy/UserServiceImp.java
@@ -1,6 +1,6 @@
 package com.uu.a_proxy.jdk_proxy;
 
-public class UserServiceImp{
+public class UserServiceImp implements UserService{
 
 	public void eat() {
 		System.out.println("-------吃面包------");
